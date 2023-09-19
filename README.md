c63b090bf8231209bd11298ff52ef271f81aaf27
Manjesh Singh <smanjesh1854@gmail.com>
2023-09-19 14:25:55 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vite + React + SCSS 
+## Starter template
+### Centered, background black, font gray, font-family: Poppins, weight 100 - 700, uses rem
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.scss'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Hello</h1>
+      <a href="#">Not going anywhere</a>
+      <br />
+      <button>Click</button>
+    </div>
+  )
+}
+
+export default App
